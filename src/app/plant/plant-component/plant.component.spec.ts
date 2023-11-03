d916a1a913353fdc080228eeb9d780c1180c1a85
Andres Losada <nedrocoli@gmail.com>
2023-11-02 23:57:36 -0500

    Adding and fixign tests

--- src/app/plant/plant-component/plant.component.spec.ts
+++ src/app/plant/plant-component/plant.component.spec.ts
@@ -1,27 +1,61 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { By } from '@angular/platform-browser'
 import { DebugElement } from '@angular/core'
+import { faker } from '@faker-js/faker'
 
+import { RouterTestingModule } from '@angular/router/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { Plant } from '../plant'
 import { PlantComponent } from './plant.component'
+import { PlantService } from './plant.service'
+import { AppComponent } from 'src/app/app.component'
 
-describe('PlantComponent', () => {
+describe('DirectorListComponent', () => {
   let component: PlantComponent
   let fixture: ComponentFixture<PlantComponent>
+  let debug: DebugElement
+  let plantAmount: number
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [PlantComponent]
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [AppComponent, PlantComponent],
+      providers: [PlantService]
     }).compileComponents()
   }))
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantComponent)
     component = fixture.componentInstance
+    plantAmount = faker.number.int({ min: 5, max: 20 })
+    const fakeType = ['Exterior', 'Interior']
+    const fakeplants = Array(plantAmount)
+      .fill(0)
+      .map(
+        () =>
+          new Plant(
+            faker.number.int(),
+            faker.lorem.words(),
+            faker.lorem.words(),
+            fakeType[faker.number.int({ min: 0, max: 1 })],
+            faker.number.int(),
+            faker.lorem.word(),
+            faker.lorem.words()
+          )
+      )
+    component.plants = fakeplants
+
     fixture.detectChanges()
+    debug = fixture.debugElement
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('Component has X amount of plants', () => {
+    expect(debug.query(By.css('#plantList')).childNodes.length -1).toBe(
+      plantAmount
+    )
   })
 })
--- src/app/plant/plant-component/plant.service.spec.ts
+++ src/app/plant/plant-component/plant.service.spec.ts
@@ -1,16 +1,18 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { PlantService } from './plant.service';
+import { TestBed, async, inject } from '@angular/core/testing'
+import { PlantService } from './plant.service'
+import { HttpClientModule } from '@angular/common/http'
 
 describe('Service: Plant', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [PlantService]
-    });
-  });
+    })
+  })
 
   it('should ...', inject([PlantService], (service: PlantService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+    expect(service).toBeTruthy()
+  }))
+})
